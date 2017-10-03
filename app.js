bbfe76da0a7487d701d71ecea547b9a37173668b
Kien-Lai <laitrungkien27396@gmail.com>
2017-10-03 10:12:32 +0700

    fix(output)

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var request = require('request');
 var cheerio = require('cheerio');
 const bodyParser = require('body-parser');
+require('events').EventEmitter.prototype._maxListeners = 100;
 var controller = require('./controller');
 
 var app = express();
@@ -11,10 +12,15 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.post('/scrape', (req, res) => {
     const arrLink = req.body.data.arrLink;
     const query = req.body.data.query;
-    Promise.all(arrLink.map( url => controller.getTitle(url, query)))
+    const description = req.body.data.description;
+    Promise.all(arrLink.map( (url, index) => controller.getTitle(url, query, description, index)))
     .then(success => res.send(success))
     .catch(err => res.send(err));
 })
+
+if ('development' == app.get('env')) {
+    process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+}
 
 app.listen(3000, (req, res) => {
     console.log('server is runing at localhost://3000');
--- controller.js
+++ controller.js
@@ -1,20 +1,22 @@
 var request = require('request');
 var cheerio = require('cheerio');
 
-module.exports.getTitle = function(url, query) {
+module.exports.getTitle = function(url, query, description, index) {
     return new Promise((resolve, reject) => {
-        request(url, (error, response, html) => {
+        request({url: url.name, rejectUnhauthorized : false}, (error, response, html) => {
             if(error){
-                reject(error);
+                reject(`${index}`);
             }
             else {
                 var $ = cheerio.load(html);
                 resolve({
                     query: query,
+                    description: description,
                     sites: {
-                        url: url,
+                        url: url.name,
                         title: $('h1').slice(0,1).text(),
-                        content: $('p').slice(0,3).text(),
+                        content: $('p').text(),
+                        relevance: url.relevance, 
                     }
                 });
             }
